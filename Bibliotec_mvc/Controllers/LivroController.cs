4cf155addf062377e8911aacacf051b2a5c4a6c4
Elle <56184436864@info.local>
2024-12-05 16:44:48 -0300

    atualizando

--- Bibliotec_mvc/Controllers/LivroController.cs
+++ Bibliotec_mvc/Controllers/LivroController.cs
@@ -2,11 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Bibliotec.Contexts;
 using Bibliotec.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Bibliotec_mvc.Controllers
@@ -21,101 +21,246 @@ namespace Bibliotec_mvc.Controllers
             _logger = logger;
         }
 
-
-        //metodo que retorna a tela de cadastro:
-        [Route("Cadastro")]
-        public IActionResult Cadastro()
-        {
-            ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
-            ViewBag.Categorias = context.Categoria.ToList();
-            return View();
-        }
-
-        // metodo para cadastrar um livro:
-        [Route("Cadastrar")]
-        public IActionResult Cadastrar(IFormCollection form)
-        {
-            Livro novolivro = new Livro();
-
-
-            novolivro.Nome = form["Nome"].ToString();
-            novolivro.Descricao = form["Descricao"].ToString();
-            novolivro.Editora = form["Editora"].ToString();
-            novolivro.Escritor = form["Escritor"].ToString();
-            novolivro.Idioma = form["Idioma"].ToString();
-
-            //trabalhar com imagens:
-            if (form.Files.Count > 0)
-            {
-
-                var arquivo = form.Files[0];
-
-                var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwroot/imagens/Livros");
-
-                if (Directory.Exists(pasta))
-                {
-                    //criar a pasta: 
-                    Directory.CreateDirectory(pasta);
-                }
-
-                //Terceiro passo
-                //criar a variavel para armazenar o caminho em quer meu aruivo estara , alem do nome dele.
-
-                var caminho = Path.Combine(pasta, arquivo.FileName);
-                using (var stream = new FileStream(caminho, FileMode.Create))
-                {
-                    //copiou o arquivo para o meu diretorio
-                    arquivo.CopyTo(stream);
-                }
-                novolivro.Imagem = arquivo.FileName;
-            }
-            else
-            {
-                novolivro.Imagem = "padrao.png";
-            }
-            context.Livro.Add(novolivro);
-            context.SaveChanges();
-
-            //segunda parte é adicionar dentro de livrocategoria a cateforia que ´pertence ao nobo livro
-
-            List<LivroCategoria> listalivrocategorias = new List<LivroCategoria>(); //lista as categorias
-            string[] categoriasSelecionadas = form["Categoria"].ToString().Split(",");
-
-            foreach (string categoria in categoriasSelecionadas)
-            {
-                LivroCategoria livroCategoria = new LivroCategoria();
-                livroCategoria.CategoriaID = int.Parse(categoria);
-                livroCategoria.LivroID = novolivro.LivroID;
-
-            }
-            context.LivroCategoria.AddRange(listalivrocategorias);
-            context.SaveChanges();
-            return LocalRedirect("/Livro/Cadastro");
-        }
-
         Context context = new Context();
-        private object form;
 
         public IActionResult Index()
         {
             ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
 
+            //Criar uma lista de livros
+            List<Livro> listaLivros = context.Livro.ToList();
 
-            //criar uma lista de livros
-            List<Livro> ListaLivros = context.Livro.ToList();
+            //Verificar se o livro tem reserva ou não
+            //ToDictionay(chave, valor)
+            var livrosReservados = context.LivroReserva.ToDictionary(livro => livro.LivroID, livror => livror.DtReserva);
 
-
-
-
-
-            var LivroReservados = context.LivroReserva.ToDictionary(Livro => Livro.LivroID, livror => livror.DtReserva);
-
-            ViewBag.Livros = ListaLivros;
-            ViewBag.LivroComReservas = LivroReservados;
+            ViewBag.Livros = listaLivros;
+            ViewBag.LivrosComReserva = livrosReservados;
 
             return View();
         }
 
+        [Route("Cadastro")]
+        //Método que retorna a tela de cadastro:
+        public IActionResult Cadastro()
+        {
+
+            ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
+
+            ViewBag.Categorias = context.Categoria.ToList();
+            //Retorna a View de cadastro:
+            return View();
+        }
+
+        // Método para cadastrar um livro:
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+
+            //PRIMEIRA PARTE: Cadastrar um livro na tabela Livro
+            Livro novoLivro = new Livro();
+
+            //O que meu usuário escrever no formulário será atribuido ao novoLivro
+            novoLivro.Nome = form["Nome"].ToString();
+            novoLivro.Descricao = form["Descricao"].ToString();
+            novoLivro.Editora = form["Editora"].ToString();
+            novoLivro.Escritor = form["Escritor"].ToString();
+            novoLivro.Idioma = form["Idioma"].ToString();
+
+            //Trabalhar com imagens:
+
+            if (form.Files.Count > 0)
+            {
+                //Primeiro passo:
+                // Armazenaremos o arquivo/foto enviado pelo usuário
+                var arquivo = form.Files[0];
+
+                //Segundo passo:
+                //Criar variavel do caminho da minha pasta para colocar as fotos dos livros
+                var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Livros");
+                //Validaremos se a pasta que será armazenada as imagens, existe. Caso não exista, criaremos uma nova pasta.
+                if (!Directory.Exists(pasta))
+                {
+                    //Criar a pasta:
+                    Directory.CreateDirectory(pasta);
+                }
+                //Terceiro passo:
+                //Criar a variavel para armazenar o caminho em que meu arquivo estara, além do nome dele
+                var caminho = Path.Combine(pasta, arquivo.FileName);
+
+                using (var stream = new FileStream(caminho, FileMode.Create))
+                {
+                    //Copiou o arquivo para o meu diretório
+                    arquivo.CopyTo(stream);
+                }
+
+                novoLivro.Imagem = arquivo.FileName;
+            }
+            else
+            {
+                novoLivro.Imagem = "padrao.png";
+            }
+
+
+            context.Livro.Add(novoLivro);
+            context.SaveChanges();
+
+            // SEGUNDA PARTE: É adicionar dentro de LivroCategoria a categoria que pertence ao novoLivro
+            //Lista a tabela LivroCategoria:
+            List<LivroCategoria> listaLivroCategorias = new List<LivroCategoria>();
+
+            //Array que possui as categorias selecionadas pelo usuário
+            string[] categoriasSelecionadas = form["Categoria"].ToString().Split(',');
+            //Ação, terror, suspense
+            //3, 5, 7
+
+            foreach (string categoria in categoriasSelecionadas)
+            {
+                //string categoria possui a informação do id da categoria ATUAL selecionada.
+                LivroCategoria livroCategoria = new LivroCategoria();
+
+                livroCategoria.CategoriaID = int.Parse(categoria);
+                livroCategoria.LivroID = novoLivro.LivroID;
+                //Adicionamos o obj livroCategoria dentro da lista listaLivroCategorias
+                listaLivroCategorias.Add(livroCategoria);
+            }
+
+            //Peguei a coleção da listaLivroCategorias e coloquei na tabela LivroCategoria
+            context.LivroCategoria.AddRange(listaLivroCategorias);
+
+            context.SaveChanges();
+
+            return LocalRedirect("/Livro/Cadastro");
+
+        }
+
+
+        [Route("Editar/{id}")]
+        public IActionResult Editar(int id)
+        {
+
+            ViewBag.Admin = HttpContext.Session.GetString("Admin")!;
+
+            ViewBag.CategoriasDoSistema = context.Categoria.ToList();
+
+            // LivroID == 3
+
+            //Buscar quem é o tal do id numero 3:
+            Livro livroEncontrado = context.Livro.FirstOrDefault(livro => livro.LivroID == id)!;
+
+            //Buscar as categorias que o livroEncontrado possui
+            var categoriasDoLivroEncontrado = context.LivroCategoria
+            .Where(identificadorLivro => identificadorLivro.LivroID == id)
+            .Select(livro => livro.Categoria)
+            .ToList();
+
+            //Quero pegar as informaçoes do meu livro selecionado e mandar para a minha View
+            ViewBag.Livro = livroEncontrado;
+            ViewBag.Categoria = categoriasDoLivroEncontrado;
+
+
+
+            return View();
+        }
+
+        //Método que atualiza as informações do Livro:
+        [Route("Atualizar/{id}")]
+        public IActionResult Atualizar(IFormCollection form, int id, IFormFile imagem)
+        {
+            //Buscar um livro especifico pelo ID
+            Livro livroAtualizado = context.Livro.FirstOrDefault(livro => livro.LivroID == id)!;
+
+            livroAtualizado.Nome = form["Nome"];
+            livroAtualizado.Escritor = form["Escritor"];
+            livroAtualizado.Editora = form["Editora"];
+            livroAtualizado.Idioma = form["Idioma"];
+            livroAtualizado.Descricao = form["Descricao"];
+
+            //Upload de imagem
+            if (imagem != null && imagem.Length > 0)
+            {
+                //Definir o caminho da minha imagem do livro ATUAL, que eu quero alterar:
+                var caminhoImagem = Path.Combine("wwwroot/images/Livros", imagem.FileName);
+
+                //Verificar se o usuario colocou uma imagem para atualizar o livro
+                if (!string.IsNullOrEmpty(livroAtualizado.Imagem))
+                {
+                    //Caso exista, ela irá ser apagada
+                    var caminhoImagemAntiga = Path.Combine("wwwroot/images/Livros", livroAtualizado.Imagem);
+                    //Ver se existe uma imagem no caminho antigo
+                    if (System.IO.File.Exists(caminhoImagemAntiga))
+                    {
+                        System.IO.File.Delete(caminhoImagemAntiga);
+                    }
+                }
+
+                //Salvar a imagem nova
+                using (var stream = new FileStream(caminhoImagem, FileMode.Create))
+                {
+                    imagem.CopyTo(stream);
+                }
+
+                //Subir essa mudança para o meu banco de dados
+                livroAtualizado.Imagem = imagem.FileName;
+            }
+
+            //CATEGORIAS:
+            //PRIMEIRO: Precisamos pegar as categorias selecionadas do usuário
+            var categoriasSelecionadas = form["Categoria"].ToList();
+            //SEGUNDO: Pegaremos as categorias ATUAIS do livro
+            var categoriasAtuais = context.LivroCategoria.Where(livro => livro.LivroID == id).ToList();
+            //TERCEIRO: Removeremos as categorias antigas
+            foreach (var categoria in categoriasAtuais)
+            {
+                if (!categoriasSelecionadas.Contains(categoria.CategoriaID.ToString()))
+                {
+                    //Nos vamos remover a categoria do nosso context
+                    context.LivroCategoria.Remove(categoria);
+                }
+            }
+            //QUARTO: Adicionaremos as novas categorias
+            foreach (var categoria in categoriasSelecionadas)
+            {
+                //Verificando se nao existe a categoria nesse livro
+                if (!categoriasAtuais.Any(c => c.CategoriaID.ToString() == categoria))
+                {
+                    context.LivroCategoria.Add(new LivroCategoria
+                    {
+                        LivroID = id,
+                        CategoriaID = int.Parse(categoria)
+                    });
+                }
+            }
+
+            context.SaveChanges();
+
+            return LocalRedirect("/Livro");
+
+
+        }
+
+        [Route("Excluir")]
+        public IActionResult Excluir(int id)
+        {
+
+            //Buscar
+            Livro livroEncontrado = context.Livro.First(Livro => Livro.LivroID == id);
+
+
+            //buscar as categorias desse lviro:
+            var categoriasDoLivro = context.LivroCategoria.Where(livro => livro.LivroID == id).ToList();
+
+            //presica exlior primeiro o registro da tabela intermediaria
+            foreach (var categoria in categoriasDoLivro)
+            {
+                context.LivroCategoria.Remove(categoria);
+            }
+
+            context.Livro.Remove(livroEncontrado);
+
+            context.SaveChanges();
+            return LocalRedirect("/Livro");
+        }
         // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         // public IActionResult Error()
         // {
